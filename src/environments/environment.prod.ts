713c0e178247451b7af7e4263a8885183fe5ba8f
Hugo Lignères <49685716+hugolgs-dev@users.noreply.github.com>
2025-03-23 19:53:24 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://revisemate-580ab153bf6d.herokuapp.com'
+  apiUrl: 'https://revisemate-580ab153bf6d.herokuapp.com/api', // Ensure this is correct
 };
