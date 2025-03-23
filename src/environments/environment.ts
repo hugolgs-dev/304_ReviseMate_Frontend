f360a862a0d602ea4743630cdd7d2fab3c86deb0
hugolgs-dev <hugo.ligneres@orange.fr>
2025-03-23 20:36:24 +0100

    fix url dans environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://revisemate-580ab153bf6d.herokuapp.com/api',
+  apiUrl: 'https://revisemate-580ab153bf6d.herokuapp.com',
   url: 'https://revisemate-580ab153bf6d.herokuapp.com',
 };
